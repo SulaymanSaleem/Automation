12ab15751aad2fbc2fc1c3d42076e748cf683184
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2020-02-05 15:15:21 +0000

    added vm-map-count command

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -8,6 +8,7 @@ echo "################## Pulling github repo ###################"
 ### including vm.mac.count in systemctl.conf file ###
 
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+sysctl -w vm.max_map_count=262144
 git clone https://github.com/SulaymanSaleem/elastic-cluster
 
 cd elastic-cluster
